a8231847aff6887122e68bcf7dc3cbc93d24d64a
Anderson Bravalheri <andersonbravalheri@gmail.com>
2022-02-26 11:54:17 +0000

    Expose `format_pyproject` and `PyProjectFmtNamespace` as part of the API

--- src/pyproject_fmt/__init__.py
+++ src/pyproject_fmt/__init__.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
 from ._version import version as __version__
+from .cli import PyProjectFmtNamespace
+from .formatter import format_pyproject
 
 __all__ = [
     "__version__",
+    "format_pyproject",
+    "PyProjectFmtNamespace",
 ]
