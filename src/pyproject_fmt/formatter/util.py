8ab3f8a893b1b254a0ae0afc2debee03871a5e06
Adam Johnson <me@adamj.eu>
2023-02-25 10:01:26 +0000

    ignore coverage

--- src/pyproject_fmt/formatter/util.py
+++ src/pyproject_fmt/formatter/util.py
@@ -114,7 +114,9 @@ def ensure_newline_at_end(body: Table) -> None:
             content = content[-1]
         elif isinstance(content, Table) and content.value.body and isinstance(content.value.body[-1][1], (AoT, Table)):
             content = content.value.body[-1][1]
-        else:
+        else:  # pragma: no cover
+            # coverage has a bug on python < 3.10, seeing this line as uncovered
+            # https://github.com/nedbat/coveragepy/issues/1480
             break
     whitespace = Whitespace("\n")
     insert_body = content.value.body
